4851f95444c553096c37610f14d8e1007f96e40c
renjwjx <renjwjx@gmail.com>
2016-11-26 21:01:18 +0800

    Initial Commit

---
+++ FastSearchJabber/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FastSearchJabber
+//
+//  Created by jinren on 26/11/2016.
+//  Copyright © 2016 jinren. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
